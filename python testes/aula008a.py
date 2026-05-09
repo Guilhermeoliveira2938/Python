c25add47906f0de77eb154e5c0198be860fe2643
Guilherme <guilhermeoliveira2938@gmail.com>
2026-05-09 20:29:36 -0300

    python exercicios

---
+++ python testes/aula008a.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número: "))
+import math
+raiz = math.sqrt(num)
+potencia = math.pow(num,2)
+print("A raiz do seu número é: {} \nE a quadrado do seu número é: {}".format(raiz,potencia))
